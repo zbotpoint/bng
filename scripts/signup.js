5d1ca471ac7dbc34f77de04c14a226b9732f2adf
Zachary LaPointe <zachary.lapointe@gmail.com>
2020-08-09 00:31:08 -0400

    Adding signup template

---
+++ scripts/signup.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+   $form = $('form');
+   $form.on("submit",function (e) {
+       if (e) e.preventDefault();
+       send($form);
+   })
+});
+
+function send($form) {
+    $.ajax({
+        type: $form.attr('method'),
+        url: $form.attr('action'),
+        data: $form.serialize(),
+        cache: false,
+        dataType: 'json',
+        contentType: "application/json; charset=utf-8",
+        error: function(err) { alert("Could not connect to the mailchimp server. Please try again later or email us at blueandgold@skule.ca."); },
+        success: function(data) {
+            if (data.result !== "success") {
+                console.log("Error - email address already signed up.")
+            }
+        }
+    });
+}
