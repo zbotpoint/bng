d7acb4de3d87180ad09a1d4f6bbc9f3dbf1f5f55
Zachary LaPointe <zachary.lapointe@gmail.com>
2020-08-09 23:29:41 -0400

    Adding WIP events page

--- scripts/dusted.js
+++ scripts/dusted.js
@@ -6,8 +6,8 @@ $(document).ready(function () {
         else {
             $(".dusted .btn").removeClass("disabled").prop("disabled",false);
         }
-    })
+    });
     document.getElementById("submitter").addEventListener('click', event => {
         $("#submit-url").prop('href',"https://docs.google.com/forms/d/e/1FAIpQLScGlUSBhFhqPzr5SYx-1LqE5H4bGlAKYJibGwL3crZHtINCjQ/viewform?usp=pp_url&entry.2007659015="+encodeURIComponent($(".field").val()))[0].click();
-    })
-}
+    });
+});
